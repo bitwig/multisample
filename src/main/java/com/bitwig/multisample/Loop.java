cc21f211355693d226ca333bbe86dc1c61c5579d
Claes Johanson <claes@bitwig.com>
2021-06-04 12:55:44 +0200

    Gradle wrapper (#1)
    * Updated comments and added schema generation to pom
    
    * Added gradle build settings

--- src/main/java/com/bitwig/multisample/Loop.java
+++ src/main/java/com/bitwig/multisample/Loop.java
@@ -4,17 +4,21 @@ import jakarta.xml.bind.annotation.XmlAttribute;
 
 public class Loop
 {
-   /** Loop mode - off/loop/ping-pong */
+   /** 
+    * Loop mode - off/loop/ping-pong 
+    */
    @XmlAttribute
    public LoopMode mode;
 
-   /** Loop start in samples.
+   /** 
+    * Loop start in samples.
     * Fractional positions are allowed and can be used for loop tuning.
     * */
    @XmlAttribute
    public Double start;
 
-   /** Loop stop (end) in samples.
+   /** 
+    * Loop stop (end) in samples.
     * Fractional positions are allowed and can be used for loop tuning.
     * */
    @XmlAttribute
